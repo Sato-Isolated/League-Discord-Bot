585dbb344f3330ffe6bc7c725ad2372fee66b6bf
MindLated <ismyskyllz@gmail.com>
2024-01-23 05:09:58 +0100

    Added MakeCsv Function
    I've added the MakeCsv function
    
    which allows you to retrieve games (ARAM here) from one date to another up to the present day, showing the number of times you've had a champion, as well as the number of wins and losses, kills , deaths , assists, ect.

--- Modules/LeagueCommand.cs
+++ Modules/LeagueCommand.cs
@@ -1,11 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Reflection;
 using System.Text;
 using Camille.Enums;
 using Camille.RiotGames;
 using Camille.RiotGames.Enums;
+using CsvHelper;
 using Discord;
 using Discord.Interactions;
+using Newtonsoft.Json;
 
 namespace League_Discord_Bot.Modules;
 
@@ -104,7 +107,176 @@ public class LeagueCommand : InteractionModuleBase<SocketInteractionContext>
                ?? string.Empty;
     }
 
+    [SlashCommand("makecsv", "blablabla csv aram")]
+    public async Task makecsv(string text, string tagline)
+    {
+        try
+        {
+            await RespondAsync("Preparation", ephemeral: true);
+            var summs = await Api.AccountV1().GetByRiotIdAsync(RegionalRoute.EUROPE, text, tagline);
+            var puuid = await Api.SummonerV4().GetByPUUIDAsync(PlatformRoute.EUW1, summs.Puuid);
 
+            DateTime starttime = new DateTime(2023, 12, 31);
+            DateTime endtime = new DateTime(2024, 1, 1);
+            DateTime endtimefinal = DateTime.Today;
+            var FilePath = $"{text}.csv";
+            var stats = new List<GameStats>();
+
+            var pathFolder = Path.Combine("Game", text);
+
+            Directory.CreateDirectory(pathFolder);
+            var nbgame = 0;
+            var spectFile = Path.Combine(pathFolder, "games.txt");
+            var gameAlreadyCheck = File.Exists(spectFile) ? (await File.ReadAllLinesAsync(spectFile)).ToList() : new List<string>();
+            var nbentries = 0;
+            while (starttime <= endtimefinal)
+            {
+                long starttamp = ((DateTimeOffset)starttime.ToUniversalTime()).ToUnixTimeSeconds();
+                long endtamp = ((DateTimeOffset)endtime.ToUniversalTime()).ToUnixTimeSeconds();
+                var leagueentries = await Api.MatchV5().GetMatchIdsByPUUIDAsync(RegionalRoute.EUROPE, puuid.Puuid, 100, endtamp, Queue.HOWLING_ABYSS_5V5_ARAM, starttamp);
+                nbentries += leagueentries.Length;
+        
+                if (File.Exists(FilePath))
+                {
+                    using (var reader = new StreamReader(FilePath))
+                    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                    {
+                        stats = csv.GetRecords<GameStats>().ToList();
+                    }
+                }
+
+                foreach (var spect in leagueentries)
+                {
+                    nbgame += 1;
+                    await ModifyOriginalResponseAsync(properties =>
+                    {
+                        properties.Content = $"{nbgame} / {nbentries}";
+                    });
+                    await Task.Delay(1000);
+                    if (gameAlreadyCheck.Contains(spect))
+                    {
+                        continue;
+                    }
+                    var match = await Api.MatchV5().GetMatchAsync(RegionalRoute.EUROPE, spect);
+                    if (match is not null)
+                    {
+                        var gameMatch = Path.Combine(pathFolder, $"{spect}.json");
+                        await File.WriteAllTextAsync(gameMatch, JsonConvert.SerializeObject(match));
+
+                        var participant = match.Info.Participants.Single(x => x.RiotIdGameName == text);
+                        var winloose = participant.Win;
+                        var firstBlood = participant.FirstBloodKill;
+                        var kill = participant.Kills;
+                        var death = participant.Deaths;
+                        var assist = participant.Assists;
+                        var champ = participant.ChampionName;
+                        var damage = participant.TotalDamageDealtToChampions;
+                        var duration = TimeSpan.FromSeconds(match.Info.GameDuration);
+                        var dpm = (double)damage / duration.Minutes;
+                        var dk = participant.DoubleKills;
+                        var tk = participant.TripleKills;
+                        var qk = participant.QuadraKills;
+                        var pk = participant.PentaKills;
+
+                        var newStats = new GameStats
+                        {
+                            Champ = champ,
+                            Game = 1,
+                            WinRate = winloose ? 100 : 0,
+                            Win = winloose ? 1 : 0,
+                            Loose = winloose ? 0 : 1,
+                            FirstBlood = firstBlood ? 1 : 0,
+                            KDA = death == 0 ? kill + assist : (kill + assist) / (double)death,
+                            Kill = kill,
+                            Death = death,
+                            Assist = assist,
+                            DPM = dpm,
+                            DoubleKill = dk,
+                            TripleKill = tk,
+                            QuadraKill = qk,
+                            PentaKill = pk
+                        };
+
+                        var oldStats = stats.FirstOrDefault(s => s.Champ == newStats.Champ);
+                        if (oldStats != null)
+                        {
+                            oldStats.Game += 1;
+
+                            if (winloose)
+                                oldStats.Win += 1;
+                            else
+                                oldStats.Loose += 1;
+
+                            var numOfGames = oldStats.Win + oldStats.Loose;
+                            var winRate = oldStats.Win / (float)numOfGames * 100;
+                            oldStats.WinRate = Convert.ToDouble(winRate.ToString("F1"));
+
+                            if (firstBlood is true)
+                                oldStats.FirstBlood += 1;
+
+                            oldStats.Kill += kill;
+                            oldStats.Death += death;
+                            oldStats.Assist += assist;
+                            var kda = (oldStats.Kill + oldStats.Assist) / (double)oldStats.Death;
+                            oldStats.KDA = Convert.ToDouble(kda.ToString("F1"));
+                            oldStats.DPM = Convert.ToDouble(((oldStats.DPM * (oldStats.Game - 1) + dpm) / oldStats.Game).ToString("F1"));
+                            oldStats.DoubleKill += dk;
+                            oldStats.TripleKill += tk;
+                            oldStats.QuadraKill += qk;
+                            oldStats.PentaKill += pk;
+                        }
+                        else
+                        {
+                            stats.Add(newStats);
+                        }
+                        gameAlreadyCheck.Add(spect);
+                    }
+                }
+
+                starttime = starttime.AddDays(1);
+                endtime = endtime.AddDays(1);
+            }
+
+            await File.WriteAllLinesAsync(spectFile, gameAlreadyCheck);
+
+            using (var writer = new StreamWriter(FilePath))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                await csv.WriteRecordsAsync(stats);
+            }
+            await ModifyOriginalResponseAsync(properties =>
+            {
+                properties.Content = "Finish";
+            });
+        }
+        catch (Exception ex)
+        {
+            await ModifyOriginalResponseAsync(properties =>
+            {
+                properties.Content =  ex.ToString();
+            });
+        }
+    }
+
+    public class GameStats
+    {
+        public string Champ { get; set; }
+        public int Game { get; set; }
+        public double WinRate { get; set; }
+        public int Win { get; set; }
+        public int Loose { get; set; }
+        public int FirstBlood { get; set; }
+        public double KDA { get; set; }
+        public int Kill { get; set; }
+        public int Death { get; set; }
+        public int Assist { get; set; }
+        public int DoubleKill { get; set; }
+        public int TripleKill { get; set; }
+        public int QuadraKill { get; set; }
+        public int PentaKill { get; set; }
+
+        public double DPM { get; set; }
+    }
 
     [SlashCommand("lg", "Check les stats de la game en cours")]
     public async Task LiveGame(string text, string tagline)
